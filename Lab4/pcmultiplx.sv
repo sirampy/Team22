481a44888ba0cb6674724407693db6a138b61e79
Slayque3n <soph.jayson@gmail.com>
2023-11-17 11:38:35 +0000

    yay

---
+++ Lab4/pcmultiplx.sv
@@ -0,0 +1,10 @@
+module pcmultiplx(
+    input logic branch_PC,
+    input logic PCsrc,
+    input logic inc_PC,
+    output logic next_PC
+);
+
+assign next_PC = PCsrc ? branch_PC:inc_PC;
+
+endmodule
