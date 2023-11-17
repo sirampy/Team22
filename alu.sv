f520d4d0943b57880bccc801673b2c8fc01a90ef
root <root@beth>
2023-11-17 11:04:00 +0000

    create alu file

---
+++ alu.sv
@@ -0,0 +1,13 @@
+module alu #(
+    parameter WIDTH = 16
+)(
+    input  logic     ALUop1,      // clock 
+    input  logic     ALUop2,      // reset
+    input  logic     ALUctrl,
+    output logic     SUM,
+    output logic     EQ  // output
+);
+
+
+
+endmodule
