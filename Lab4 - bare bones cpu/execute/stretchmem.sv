629cfe00dd1b7ff77648f754c18122f26de70435
Slayque3n <soph.jayson@gmail.com>
2023-11-24 11:53:18 +0000

    extensiom

---
+++ Lab4 - bare bones cpu/execute/stretchmem.sv
@@ -0,0 +1,16 @@
+module strechmem #(
+    ADRESS_WIDTH =32,
+    DATA_WIDTH=8
+)(
+    input logic clk,
+    input logic [ADRESS_WIDTH-1:0] addr,
+    output logic [DATA_WDTH - 1:0] doubt);
+logic [DATA_WDTH - 1:0] rom_array [2**ADRESS_WIDTH -1:0];
+
+initial begin
+    $("sinerom.mem", rom_array);
+
+end;
+always_ff@(posedge clk)
+    dout<= rom_array [addr];
+endmodule
