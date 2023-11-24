f8311fed772947ec3ae294630155ddc5b650c4ba
root <root@beth>
2023-11-24 10:33:46 +0000

    Updated ALU.sv

--- Lab4 - bare bones cpu/execute/ALU.sv
+++ Lab4 - bare bones cpu/execute/ALU.sv
@@ -1,19 +1,18 @@
 module alu (
     input  logic [7:0]    aluOp1,      // RD1 
     input  logic [7:0]    aluOp2,      // either RD2 or imm
-    input  logic [3:0]    aluCtrl,     // alu selection of instruction
+    input  logic [2:0]    aluCtrl,     // alu selection of instruction
     output logic [7:0]    sum,         // output
-    output logic          eq           // equal flag
+    output logic          eq           // equal/zero flag
 );
 
 always_comb 
     case (aluCtrl)
-        4'b0000: sum = aluOp1 + aluOp2;                     // add
-        4'b1111: eq = (aluOp1 == aluOp2) ? 1'b0 : 1'b1;     // bne
-        default: begin
-            sum = 0;
-            eq = 0;
-        end
+        3'b000: sum = aluOp1 + aluOp2;                     // add
+        3'b001: sum = aluOp1 - aluOp2;                     // subtract
+        default: sum = 0;
     endcase    
+
+assign eq = (sum == 0) ? 1'b1 : 1'b0;           //outputs 1 if equal
 
 endmodule
