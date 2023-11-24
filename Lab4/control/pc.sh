8acae3c878252e19a41d4b6d5a52a656681a324c
Sirampy <alex.ch4.cl@gmail.com>
2023-11-24 15:54:08 +0000

    added mems

--- Lab4/control/pc.sh
+++
@@ -1,7 +0,0 @@
-rm -rf obj_dir
-rm -f pc_top.vcd
-
-verilator -Wall --cc --trace pc_top.sv pc_reg.sv pc_multiplx.sv --exe pc_tb.cpp
-make -j -C obj_dir/ -f Vpc_top.mk Vpc_top
-
-obj_dir/Vpc_top
---
+++ Lab4/rom_data/generate.py
@@ -0,0 +1,12 @@
+import math
+import string
+f = open("datamem.mem","w")
+for i in range(256):
+    v = int(math.cos(2*3.1416*i/256)*127+127)
+    if (i+1)%16 == 0:
+        s = "{hex:2X}\n"
+    else:
+        s = "{hex:2X} "
+    f.write(s.format(hex=v))
+
+f.close()
