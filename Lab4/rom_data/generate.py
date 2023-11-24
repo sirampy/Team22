92ff9deab4ae0866172d4edb81358e63ccd97e79
Sirampy <alex.ch4.cl@gmail.com>
2023-11-24 15:46:18 +0000

    added memories

--- Lab4/rom_data/generate.py
+++ Lab4/rom_data/generate.py
@@ -1,6 +1,6 @@
 import math
 import string
-f = open("datamem.mem","w")
+f = open("sinerom.mem","w")
 for i in range(256):
     v = int(math.cos(2*3.1416*i/256)*127+127)
     if (i+1)%16 == 0:
@@ -10,3 +10,5 @@ for i in range(256):
     f.write(s.format(hex=v))
 
 f.close()
+
+# can't assemble from this script until a propper assembly toolchain is setup
