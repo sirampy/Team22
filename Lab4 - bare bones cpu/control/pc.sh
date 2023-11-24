00e8d61fb10beac8e3e036e3675daf5259587aa9
Slayque3n <soph.jayson@gmail.com>
2023-11-24 11:25:49 +0000

    name

--- Lab4 - bare bones cpu/control/pc.sh
+++ Lab4 - bare bones cpu/control/pc.sh
@@ -2,10 +2,6 @@ rm -rf obj_dir
 rm -f pc_top.vcd
 
 verilator -Wall --cc --trace pc_top.sv pc_reg.sv pc_multiplx.sv --exe pc_tb.cpp
-<<<<<<< HEAD
 make -j -C obj_dir/ -f Vpc_top.mk Vpc_top
-=======
-make -j -C obj_dir/ -f Vpctop.mk Vpctop
->>>>>>> 0def940533e9b61ece8b69008b20372cf1ff84fa
 
 obj_dir/Vpc_top
