24369dd80af3af83e9bfc8b79a849b0a6a9ed3af
root <root@beth>
2023-11-24 11:21:37 +0000

    Added parameters to alu_top.sv

--- Lab4 - bare bones cpu/execute/alu_top.sv
+++ Lab4 - bare bones cpu/execute/alu_top.sv
@@ -1,19 +1,22 @@
-module alu_top (
-    input logic     clk,        // clock
-    input logic     rs1,        // address of register 1
-    input logic     rs2,        // address of register 2
-    input logic     rd, 
-    input logic     RegWrite,   // write enable
-    input logic     ImmOp,      // immediate value
-    input logic     aluSrc,     // selects second input
-    input logic     aluCtrl,
-    output logic    eq,
+module alu_top #(
+    param ADDR_WIDTH = 5;
+    param DATA_WIDTH = 32;
+)(
+    input logic                   clk,        // clock
+    input logic [ADDR_WIDTH:0]    rs1,        // address of register 1
+    input logic [ADDR_WIDTH:0]    rs2,        // address of register 2
+    input logic [ADDR_WIDTH:0]    rd,         // address of file to be written to
+    input logic                   RegWrite,   // write enable
+    input logic [DATA_WIDTH:0]    ImmOp,      // immediate value
+    input logic                   aluSrc,     // selects second input
+    input logic [2:0]             aluCtrl,    // determines operation in alu
+    output logic                  eq          // equal flag
 );
 
-logic aluOp1;   // input 1
-logic aluOp2;   // input 2
-logic aluOut;   // output of ALU
-logic regOp2;   // second register
+logic [DATA_WIDTH:0] aluOp1;   // input 1
+logic [DATA_WIDTH:0] aluOp2;   // input 2
+logic [DATA_WIDTH:0] aluOut;   // output of ALU
+logic [DATA_WIDTH:0] regOp2;   // second register
 
 reg_file regfile (
     .clk (clk),
