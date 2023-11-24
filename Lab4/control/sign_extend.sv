9f7ab63ea5a418aac398e037d34c31eb23f9e47d
Dell Saxena <dellsaxena2@gmail.com>
2023-11-24 21:21:18 +0000

    debugged sign_extend.sv - fully working

--- Lab4/control/sign_extend.sv
+++ Lab4/control/sign_extend.sv
@@ -1,16 +1,20 @@
-module sign_extend (
-    input   [31 : 7]    instr,
+module sign_extend #(
+    parameter DATA_WIDTH = 32,
+              IMM_WIDTH = 12
+)(
+    input logic clk,
+    input   [DATA_WIDTH-1:0] instr,
     input   [1:0]       imm_src,
-    output reg [11:0]   imm_ext  // Declare imm_ext as reg since it's driven by procedural code
+    output reg [IMM_WIDTH-1:0] imm_ext 
 );
 
-    always_comb begin  // Enclose the case statement inside an always_comb block
+    always_comb begin  
         case (imm_src)
             2'b00 : imm_ext = {{20{instr[31]}}, instr[31:20]};
             2'b01 : imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
             2'b10 : imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-            default : imm_ext = 12'b0;  // Use 12'b0 to match the bit width of imm_ext
+            default : imm_ext = 12'b0;  
         endcase
     end
 
-endmodule
+endmodule 
