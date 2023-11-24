0def940533e9b61ece8b69008b20372cf1ff84fa
Slayque3n <soph.jayson@gmail.com>
2023-11-24 11:11:05 +0000

    change name

--- Lab4 - bare bones cpu/control/pc.sh
+++ Lab4 - bare bones cpu/control/pc.sh
@@ -1,7 +1,7 @@
 rm -rf obj_dir
 rm -f pctop.vcd
 
-verilator -Wall --cc --trace pctop.sv PCreg.sv pcmultiplx.sv --exe pc_tb.cpp
+verilator -Wall --cc --trace pc_top.sv pc_reg.sv pc_multiplx.sv --exe pc_tb.cpp
 make -j -C obj_dir/ -f Vpctop.mk Vpctop
 
 obj_dir/Vpctop
--- Lab4 - bare bones cpu/execute/execute.sh
+++ Lab4 - bare bones cpu/execute/execute.sh
@@ -1,7 +1,7 @@
 rm -rf obj_dir
 rm -f topALU.vcd
 
-verilator -Wall --cc --trace  topALU.sv --exe topALU_tb.cpp
+verilator -Wall --cc --trace  alu_rop.sv --exe topALU_tb.cpp
 
 make -j -C obj_dir/ -f VtopALU.mk VtopALU
 
