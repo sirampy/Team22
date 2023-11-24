db1d795625ed500449fe022b1c1a372f52c014b5
Dell Saxena <dellsaxena2@gmail.com>
2023-11-24 18:59:14 +0000

    updated top.sv - includes all top modules

--- Lab4/top.sv
+++ Lab4/top.sv
@@ -1,57 +1,77 @@
-module top(
-    parameter ADDR_WDTH = 32,
-              DATA_WDTH = 32
+module top #(
+    parameter ADDR_WIDTH = 5,  //address width for reg
+              DATA_WIDTH = 32, //
 )(
-    input logic         clk,
-    input logic         rst,
-    input logic     aluSrc,    
-    output logic [ADDRESS_WIDTH-1:0] a0,
-    output logic we3,
+    input logic clk,      
+    input logic rst,
+    output logic reg_write,
+    output logic [DATA_WIDTH-1:0] a0,
+    output logic reg_write,
     output logic eq,
-    output logic aluSrc,
-    output logic[2:0] aluCtrl,
-);  
-    logic [4:0] ad1; //rs1
-    logic [4:0] ad2,//rs2
-    logic [4:0] ad3, //ad3
-    logic we3;
-    logic [31:0] wd3;
-    logic eq;
-    logic PCsrc;
-    logic pc;
-    logic next_PC;
-    logic [7:0] branch_PC; 
-    logic [7:0]inc_PC;
+    output logic alu_src,
+    output logic [2:0] alu_ctrl,
+    output logic[DATA_WIDTH-1:0]  imm_op
+);
 
+    
+    logic [ADDR_WIDTH-1:0] rs1; 
+    logic [ADDR_WIDTH-1:0] rs2; 
+    logic [ADDR_WIDTH-1:0] rd;
+    logic reg_write; //write enable
+    logic alu_src; 
+    logic eq; 
+    logic pc_src; 
+    logic result_src;
+    logic mem_write;
+    logic [31:0] alu_out;
+    logic [2:0] alu_ctrl;
+    logic [31:0] imm_op;
+    logic [11:0] imm_ext
+    logic [1:0] imm_src;
+    logic [15:0] pc;
+    logic [15:0] next_pc;
+    logic [DATA_WIDTH-1:0] instr;
 
-    //instantiating ALU
-    alu alu(
-        .aluOp1(rd1),   
-        .aluOp2(aluOp2),   
-        .aluCtrl(aluCtrl),  
-        .sum(sum),     
-        .eq(eq)          
-    );
-
-    //instantiating RegFile
-    reg_file #(.ADDR_WDTH(5), .DATA_WDTH(32)) reg_file(
+    //alu+regfile
+    alu_top #(.ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(DATA_WIDTH)) alu_regfile (
         .clk(clk),
-        .AD1(ad1),
-        .AD2(ad2),
-        .AD3(ad3),
-        .WE3(we3),
-        .WD3(wd3),
-        .RD1(rd1),
-        .RD2(rd2)
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd(rd),
+        .reg_write(reg_write),
+        .imm_op(imm_op),
+        .alu_src(alu_src),
+        .alu_ctrl(alu_ctrl),
+        .eq(eq)
     );
 
-    //instantiating PC register
-    pc_reg PCreg(
+    //control: 
+    control_top control_unit (
+        .op (instr[6:0]),
+        .funct3 (instr[14:12]),
+        .funct7(instr[30]),
+        .eq(eq),
+        .pc_src(pc_src),
+        .result_src(result_src),
+        .mem_write(mem_write),
+        .alu_src(alu_src),
+        .reg_write(reg_write),
+        .alu_ctrl(alu_ctrl),
+        .imm_op(imm_op),
+        .rs1(rs1),
+        .rs2(rs2),
+        .rd(rd)
+    );
+
+    //program counter
+    pc_top pc_module (
+        .imm_op(imm_op),
         .clk(clk),
         .rst(rst),
-        .next_PC(next_PC),
-        .pc(pc)               
+        .pc_src(pc_src),
+        .pc(pc)
     );
 
-endmodule
 
+
+endmodule
