1d7c4777db53f63f87f9973293096c90648fa474
Sirampy <alex.ch4.cl@gmail.com>
2023-11-24 13:00:21 +0000

    added newline

--- Lab4/control/instr_mem.sv
+++ Lab4/control/instr_mem.sv
@@ -1,16 +1,18 @@
-modulle instr_mem (
-    input   [31:0 ] addr,
-    output  [31:0]  data
-)
+module instr_mem #(
+    parameter   ADDRESS_WIDTH = 32,
+                DATA_WIDTH = 32
+)(
+    input logic [ADDRESS_WIDTH-1:0] addr,
+    output logic [DATA_WIDTH-1:0]   dout
+);
 
 logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
 
 initial begin
         $display("loading rom.");
-        $readmemh("../rom.mem", rom_array);
+        $readmemh("../rom_bin/program.mem", rom_array);
 end;
 
-always_ff @(posedge clk)
-    dout <= rom_array[addr];
+    dout = rom_array[addr];
 
 endmodule
