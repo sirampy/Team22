18ffc3ec2ad11a0a4c9759cfa5981069d73323f9
bethcham <bethcham@gmail.com>
2023-11-25 01:42:19 +0000

    Fixed some syntax errors in alu and control

--- Lab4/alu+regfile/reg_file.sv
+++ Lab4/alu+regfile/reg_file.sv
@@ -1,35 +1,35 @@
 module reg_file #(
-    param ADDR_WDTH = 5;
-    param DATA_WDTH = 32;
-){
-    input                     clk;
+    parameter ADDR_WDTH = 5,
+    parameter DATA_WDTH = 32
+)(
+    input                     clk,
 
-    input [ADDR_WDTH - 1 : 0] AD1,
-    input [ADDR_WDTH - 1 : 0] AD2,
-    input [ADDR_WDTH - 1 : 0] AD3,
+    input [ADDR_WDTH - 1 : 0] ad1,
+    input [ADDR_WDTH - 1 : 0] ad2,
+    input [ADDR_WDTH - 1 : 0] ad3,
 
-    input                     WE3,
-    input [DATA_WIDTH - 1 : 0]WD3,
+    input                     we3,
+    input [DATA_WDTH - 1 : 0] wd3,
 
-    output [DATA_WDTH - 1 : 0]RD1,
-    output [DATA_WDTH - 1 : 0]RD2,
-};
-    logic [ (((2 ** ADDR_WDTH) -1 ) * DATA_WDTH) -1 : 0] Reg_Data;
+    output [DATA_WDTH - 1 : 0]rd1,
+    output [DATA_WDTH - 1 : 0]rd2
+);
+    logic [ ((2**ADDR_WDTH -1) * DATA_WDTH) -1 : 0] reg_data;
     // read logic
     
     always_ff @(posedge clk) begin
-
-        if (AD1 == 0)
-            RD1 [DATA_WDTH - 1 : 0] <= DATA_WIDTH'b0;
+        if (ad1 == 0)
+            rd1 [DATA_WDTH - 1 : 0] <= {DATA_WDTH{1'b0}};
         else 
-            RD1 [DATA_WDTH - 1 : 0] <= Reg_Data [(AD1 * DATA_WDTH) -1 : (AD1-1) * DATA_WDTH];
+            rd1 [DATA_WDTH - 1 : 0] <= reg_data [(ad1 * DATA_WDTH) - 1 : (ad1-1) * DATA_WDTH];
 
-        if (AD2 == 0)
-            RD2 [DATA_WDTH - 1 : 0] <= DATA_WIDTH'b0;
+        if (ad2 == 0)
+            rd2 [DATA_WDTH - 1 : 0] <= {DATA_WDTH{1'b0}};
         else 
-            RD2 [DATA_WDTH - 1 : 0] <= Reg_Data [(AD1 * DATA_WDTH) -1 : (AD2-1) * DATA_WDTH];
+            rd2 [DATA_WDTH - 1 : 0] <= reg_data [(ad2 * DATA_WDTH) -1 : (ad2-1) * DATA_WDTH];
 
-        if (WE3 && (AD3 == 'b0))
-            Reg_Data [(AD3 * DATA_WDTH ) - 1: (AD3-1) * DATA_WDTH] <= [DATA_WIDTH - 1 : 0] WD3;
+        if (we3 && (ad3 == 'b0))
+            reg_data [(ad3 * DATA_WDTH ) - 1: (ad3-1) * DATA_WDTH] <= [DATA_WDTH - 1 : 0] wd3;
     end
+
 endmodule
