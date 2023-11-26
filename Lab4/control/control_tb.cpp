4ccff48c2176483946d3ca7e971859525735a70b
Dell Saxena <dellsaxena2@gmail.com>
2023-11-26 17:25:47 +0000

    modified control_tb.cpp - adding test cases

---
+++ Lab4/control/control_tb.cpp
@@ -0,0 +1,37 @@
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    Vtop* top = new Vtop;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("top.vcd");
+
+    top->rst = 0;
+    top->pc=0;
+    top->eq=0;
+
+    for (i=0; i<1000; i++){
+
+        for (clk=0; clk<2; clk++){
+            tfp->dump (2*i + clk);
+            top->clk = !top->clk;
+            top->eval();
+        }   
+
+
+        if (Verilated::gotFinish()) 
+            exit(0);                // ... exit if finish OR 'q' pressed
+        
+
+    }
+    tfp->close();
+    exit(0);
+}
