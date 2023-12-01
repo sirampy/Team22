cf4a27ca65331ca6e6e8364e8d29fc3acab2aaf5
Slayque3n <soph.jayson@gmail.com>
2023-12-01 14:19:11 +0000

    pc and sign extend

---
+++ Project/program counter/pctop.sv
@@ -0,0 +1,22 @@
+module pc_top #(
+    parameter PC_WIDTH = 32;
+)(
+    input logic [31:0] imm_op,
+    input logic clk,
+    input logic rst,
+    input logic pc_src,
+    output logic [PC_WIDTH-1:0] pc
+);
+
+logic [PC_WIDTH-1:0] next_pc;
+
+assign next_pc = pc_src ? pc+imm_op : pc+4 ;
+
+pc_reg pc_reg (
+    .next_pc(next_pc),
+    .clk(clk),
+    .rst(rst),
+    .pc(pc)
+);
+
+endmodule
