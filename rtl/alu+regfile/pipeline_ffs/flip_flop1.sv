2bd4fa2b9abef1027c52ef75517ac5257748ea73
bethcham <bethcham@gmail.com>
2023-12-01 16:39:46 +0000

    Added the 4 pipelining flipflops

---
+++ rtl/alu+regfile/pipeline_ffs/flip_flop1.sv
@@ -0,0 +1,23 @@
+// pipelining flip flop 1 -> delays from fetch to decode
+module flip_flop1 #(
+    parameter ADDRESS_WIDTH = 32,
+              DATA_WIDTH = 32
+)(
+    input  logic                     clk_i,       // clock
+    input  logic [DATA_WIDTH-1:0]    rd_i,        // rd from instr mem
+    input  logic [ADDRESS_WIDTH-1:0] pcF_i,       // pc (fetch)
+    input  logic [ADDRESS_WIDTH-1:0] pc_plus4F_i, // pc+4 (fetch)
+
+    output logic [DATA_WIDTH-1:0]    instrD_o,    // instructions (decode)
+    output logic [ADDRESS_WIDTH-1:0] pcD_o,       // pc (decode)
+    output logic [ADDRESS_WIDTH-1:0] pc_plus4D_o  // pc+4 (decode)
+);
+
+always_ff @(posedge clk)
+    begin
+        instrD_o <= rd_i;
+        pcD_o <= pcF_i;
+        pc_plus4D_o <= pc_plus4F_i;
+    end
+
+endmodule
---
+++ rtl/alu+regfile/pipeline_ffs/flip_flop3.sv
@@ -0,0 +1,41 @@
+// pipelining flip flop 3 -> delays from execute to memory
+module flip_flop3 #(
+    parameter ADDRESS_WIDTH = 32,
+              DATA_WIDTH = 32
+)(
+    // main inputs
+    input  logic                     clk_i,         // clock
+    input logic [DATA_WIDTH-1:0]     alu_resultE_i, // alu output (execute)
+    input logic                      write_dataE_i, // data mem write enable (e)
+    input logic [11:7]               rdE_i,         // write register address (e)
+    input logic [ADDRESS_WIDTH-1:0]  pc_plus4E_i,
+
+    // control unit inputs
+    input logic reg_writeE_i,
+    input logic result_srcE_i,
+    input logic mem_writeE_i,
+
+    // main outputs
+    output logic [DATA_WIDTH-1:0]    alu_resultM_o, // alu output (memory)
+    output logic [ADDRESS_WIDTH-1:0] pcM_o,         // pc (m)
+    output logic [11:7]              rdM_o,         // write register address (m)
+    output logic [ADDRESS_WIDTH-1:0] pc_plus4M_o    // pc+4 (m)
+
+    // control unit outputs
+    output logic reg_writeM_o,
+    output logic result_srcM_o,
+    output logic mem_writeM_o,
+);
+
+always_ff @(posedge clk)
+    begin
+        alu_resultE_i <= alu_resultM_o;
+        write_dataE_i <= pcM_o;
+        rdE_i         <= rdM_o;
+        pc_plus4E_i   <= pc_plus4M_o;
+        reg_writeE_i  <= reg_writeM_o;
+        result_srcE_i <= result_srcM_o;
+        mem_writeE_i  <= mem_writeM_o;
+    end
+
+endmodule
