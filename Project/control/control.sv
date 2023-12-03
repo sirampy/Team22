cc1d3519162b3bb2a695d9258f8731a211a61c42
Dell Saxena <dellsaxena2@gmail.com>
2023-12-03 18:36:24 +0000

    rewriting control top module

---
+++ Project/control/control.sv
@@ -0,0 +1,11 @@
+module controlunit(
+    input logic [2:0]   funct3_i,
+    input logic         funct7_i,
+    input logic [6:0]   op_i,
+    output logic        mem_write,
+    output logic        alu_src,
+    
+
+    
+
+)
