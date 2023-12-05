3f0424db493fbd7147589b211fb6fc7d2c59af62
Dell Saxena <dellsaxena2@gmail.com>
2023-12-05 01:57:48 +0000

    updated sign_extend.sv

--- Project/control/sign_extend.sv
+++ Project/control/sign_extend.sv
@@ -14,7 +14,6 @@ module sign_extend #(
             2'b01 : imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]}; //store
             2'b10 : imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0}; //branch
             2'b11 : imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21, 1'b0]}; //added for JAL instruction
-
             default : imm_ext = {{20{instr[31]}}, instr[31:20]}; 
         endcase
     end
