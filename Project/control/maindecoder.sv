4716c1069742e3d46032d51112a7d58ddf88149a
Dell Saxena <dellsaxena2@gmail.com>
2023-12-05 20:12:57 +0000

    implemented jal and jalr instructions

--- Project/control/maindecoder.sv
+++ Project/control/maindecoder.sv
@@ -1,20 +1,21 @@
 module maindecoder(
-    input logic [6:0]   op_i,
-    input logic     zero_i,
-    output logic    result_src_o,
-    output logic    mem_write_o,
-    output logic    alu_src_o,
-    output logic[2:0]   imm_src_o,
-    output logic    reg_write_o,
-    output logic [1:0]  alu_op_o,
-    output logic    pc_src_o,
-    output logic    branch_o
+    input logic [6:0]   op,
+    input logic     zero,
+    output logic    result_src,
+    output logic    mem_write,
+    output logic    alu_src,
+    output logic[2:0]   imm_src,
+    output logic    reg_write,
+    output logic [1:0]  alu_op,
+    output logic    pc_src,
+    output logic    branch,
+    output logic    jump
 );
 always_comb
 case(op)
     default: begin
                 reg_write = 1'b0; //default case
-                imm_src = 2'b00;
+                imm_src = 3'b000;
                 alu_src = 1'b0;
                 mem_write = 1'b0;
                 result_src = 1'b0;
@@ -23,7 +24,7 @@ case(op)
             end
     7'b0000011: //lw intruction
                 reg_write = 1'b1;
-                imm_src = 2'b00;
+                imm_src = 3'b000;
                 alu_src = 1'b1;
                 mem_write = 1'b0;
                 result_src = 1'b1;
@@ -32,7 +33,7 @@ case(op)
 
     7'b0100011: //sw instruction
                 reg_write = 1'b0;
-                imm_src = 2'b01;
+                imm_src = 3'b001;
                 alu_src = 1'b1;
                 mem_write = 1'b1;
                 result_src = 1'b0;
@@ -40,7 +41,7 @@ case(op)
                 alu_op = 2'b00;
     7'b0110011: //r-type instruction
                 reg_write = 1'b1;
-                imm_src = 2'b00;
+                imm_src = 2'b000;
                 alu_src = 1'b0;
                 mem_write = 1'b0;
                 result_src = 1'b0;
@@ -48,28 +49,30 @@ case(op)
                 alu_op = 2'b10;
     7'b1100011: //beq instruction
                 reg_write = 1'b0;
-                imm_src = 2'b10;
+                imm_src = 3'b010;
                 alu_src = 1'b0;
                 mem_write = 1'b0;
                 result_src = 1'b0;
                 branch = 1'b1;
                 alu_op = 2'b01;
+    7'b0110111: //u-type
+                reg_write = 1'b1;
+                imm_src = 3'b100;
+                alu_src = 1'b1; 
+                mem_write = 1'b0; 
+                result_src = 2'b00; 
+                branch = 1'b0; 
+                alu_op = 2'b11;
     7'b1101111: //jump and link instruction
                 reg_write = 1'b1;
-                imm_src = 2'b11;
-                alu_src = 1'b0;
-                mem_write = 1'b0;
-                result_src = 1'b0;
-                branch = 1'b1;
-                alu_op = 2'b00;
-    7'b0110111: //load upper immediate
-                reg_write = 1'b1;
-                imm_src = ;
-                alu_src = 1'b1;
-                mem_write = 1'b0;
-                result_src = 1'b0;
-                branch = 1'b1;
-                alu_op = 2'b11;
+                imm_src = 3'b11;
+                alu_src = 1'bx; //dont care as alu isnt used
+                mem_write = 1'b0; //instruction isnt a store
+                result_src = 2'b10; //to write pc+4 into rd
+                branch = 1'b0; //not a branch
+                alu_op = 2'bxx;// dont care as alu isnt used
+
+    
 
 end case
 assign pc_src= branch&zero;
