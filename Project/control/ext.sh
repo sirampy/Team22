a8059729ece9a11708d91f3916d8cc8889ee3e5c
Dell Saxena <dellsaxena2@gmail.com>
2023-12-05 02:13:28 +0000

    updated version 2

--- Project/control/ext.sh
+++
@@ -1,8 +0,0 @@
-rm -rf obj_dir
-rm -f alu_decoder.vcd
-
-verilator -Wall --cc --trace  alu_decoder.sv --exe alu_decoder_tb.cpp
-
-make -j -C obj_dir/ -f Valu_decoder.mk Valu_decoder
-
-obj_dir/Valu_decoder
