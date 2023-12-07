31e7a1cb521565e5df2844e1ae971445897f7c70
Sirampy <alex.ch4.cl@gmail.com>
2023-12-07 15:05:36 +0000

    idk

--- revised_microarch/data_mem.sv
+++ revised_microarch/data_mem.sv
@@ -1,12 +1,12 @@
 module data_mem #(
     parameter   ADDRESS_WIDTH = 8,
-                DATA_WIDTH = 32
+                DATA_WIDTH = 8
 )(
     input logic [ADDRESS_WIDTH-1:0] a_i,
-    input logic [DATA_WIDTH-1:0]     wd_i,
+    input logic [(DATA_WIDTH*4)-1:0]    wd_i,
     input logic                     wen_i,
     
-    output logic [DATA_WIDTH-1:0]   rd_o
+    output logic [(DATA_WIDTH*4)-1:0]   rd_o
 );
 
 logic [ADDRESS_WIDTH-1:0] a_resized;
@@ -16,14 +16,23 @@ logic [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
 
 initial begin
         $display("loading rom.");
-        $readmemh("../../rom_bin/data.mem", rom_array);
+        $readmemh("rom_bin/data.mem", rom_array);
 end;
 
 always_latch begin
 
-    if (wen_i) rom_array[a_resized] = wd_i;
+    if (wen_i) begin
 
-    rd_o = rom_array[a_resized];
+        for( int i = 0; i < 4; i++) begin
+            rom_array[a_resized + i [7:0]] = wd_i [(DATA_WIDTH*(i+1))-1:(DATA_WIDTH*i)-1];
+        end
+    
+    end
+
+    rd_o = {rom_array[a_resized+3],
+        rom_array[a_resized+2],
+        rom_array[a_resized+1],
+        rom_array[a_resized]};
 end
 
 endmodule
--- revised_microarch/instr_mem.sv
+++ revised_microarch/instr_mem.sv
@@ -14,11 +14,9 @@ logic [DATA_WIDTH-1:0] rom_array [2**USED_ADDRESS_WIDTH-1:0];
 
 initial begin
         $display("loading rom.");
-        $readmemh("../../rom_bin/program.mem", rom_array);
+        $readmemh("rom_bin/program.mem", rom_array);
 end;
 
-always_comb begin
-    rd_o = rom_array[a_resized];
-end
+assign rd_o = rom_array[a_resized];
 
 endmodule
