bd806877f28f631ed920fe697de5142c751c6ba2
Dell Saxena <dellsaxena2@gmail.com>
2023-12-07 10:55:42 +0000

    main_decoder.sv updated

--- rtl/control/main_decoder.sv
+++ rtl/control/main_decoder.sv
@@ -1,59 +1,101 @@
-module main_decoder (
-    input  logic [6:0]    op_i,           // opcode
-    
-    output logic [1:0]    result_src_o,   // select write input
-    output logic          mem_write_o,    // memory write enable
-    output logic          alu_src_o,      // select rd2 or imm
-    output logic [1:0]    imm_src_o,      // imm select
-    output logic          reg_write_o,    // register write enable
-    output logic [1:0]    alu_op_o,        // to input into alu decoder
-    output logic          branch_o
+module maindecoder(
+    input logic [6:0]   op_i,
+    inout logic         zero_i,
+    output logic[1:0]    result_src_o,
+    output logic    mem_write_o,
+    output logic    alu_src_o,
+    output logic[2:0] imm_src_o,
+    output logic   reg_write_o,
+    output logic[1:0] alu_op_o,
+    output logic       pc_src_o,
+    output logic    branch_o,
+    output logic[1:0]   jump_o
 );
-
 always_comb
-    case (op_i)
-        7'b0000011: begin // load
-            reg_write_o = 1'b1;
-            imm_src_o = 2'b00;
-            alu_src_o = 1'b1;
-            mem_write_o = 1'b0;
-            result_src_o = 2'b1;
-            branch_o = 1'b0;
-            alu_op_o = 2'b00;
-        end
-        7'b0010011: begin // arithmetic operations
-            reg_write_o = 1'b1;
-            alu_src_o = 1'b0;
-            mem_write_o = 1'b0;
-            result_src_o = 1'b0;
-            branch_o = 1'b0;
-            alu_op_o = 2'b10;
-        end
-        7'b0100011: begin // store
-            reg_write_o = 1'b0;
-            imm_src_o = 2'b01;
-            alu_src_o = 1'b1;
-            mem_write_o = 1'b1;
-            branch_o = 1'b0;
-            alu_op_o = 2'b00;
-        end
-        7'b1100011: begin // branch
-            reg_write_o = 1'b0;
-            imm_src_o = 2'b10;
-            alu_src_o = 1'b0;
-            mem_write_o = 1'b0;
-            branch_o = 1'b0;
-            alu_op_o = 2'b01;
-        end
-        default: begin
-            reg_write_o = 1'b0;
-            imm_src_o = 2'b00;
-            alu_src_o = 1'b0;
-            mem_write_o = 1'b0;
-            result_src_o = 1'b0;
-            branch_o = 1'b0;
-            alu_op_o = 2'b00;
-        end
-    endcase
+case(op_i)
+    default: begin
+                reg_write_o = 1'b0; //default case
+                imm_src_o = 3'b000;
+                alu_src_o = 1'b0;
+                mem_write_o = 1'b0;
+                result_src_o = 2'b00;
+                branch_o = 1'b0;
+                alu_op_o = 2'b00;
+                jump_o = 2'b00;
+            end
+    7'b0000011: begin//lw intruction
+                reg_write_o = 1'b1;
+                imm_src_o = 3'b000;
+                alu_src_o = 1'b1;
+                mem_write_o = 1'b0;
+                result_src_o = 2'b01;
+                branch_o = 1'b0;
+                alu_op_o = 2'b00;
+                jump_o = 2'b00;
+    end
+    7'b0100011: begin //sw instruction
+                reg_write_o = 1'b0;
+                imm_src_o = 3'b001;
+                alu_src_o = 1'b1;
+                mem_write_o = 1'b1;
+                result_src_o = 2'b00;
+                branch_o = 1'b0;
+                alu_op_o = 2'b00;
+                jump_o = 2'b00;
+    end 
+    7'b0110011: begin //r-type instruction
+                reg_write_o = 1'b1;
+                imm_src_o = 3'b000;
+                alu_src_o = 1'b0;
+                mem_write_o = 1'b0;
+                result_src_o = 2'b00;
+                branch_o = 1'b0;
+                alu_op_o = 2'b10;
+                jump_o = 2'b00;
+    end 
+    7'b1100011: begin //beq instruction
+                reg_write_o = 1'b0;
+                imm_src_o = 3'b010;
+                alu_src_o = 1'b0;
+                mem_write_o = 1'b0;
+                result_src_o = 2'b00;
+                branch_o = 1'b1;
+                alu_op_o = 2'b01;
+                jump_o = 2'b00;
+    end 
+    7'b0110111: begin//u-type
+                reg_write_o = 1'b1;
+                imm_src_o = 3'b100;
+                alu_src_o = 1'b1; 
+                mem_write_o = 1'b0; 
+                result_src_o = 2'b00; 
+                branch_o = 1'b0; 
+                alu_op_o = 2'b11;
+                jump_o = 2'b00;
+    end 
+    7'b1101111: begin //jump_o and link instruction
+                reg_write_o = 1'b1;
+                imm_src_o = 3'b011;
+                alu_src_o = 1'bx; //dont care as alu isnt used
+                mem_write_o = 1'b0; //instruction isnt a store
+                result_src_o = 2'b10; //to write pc+4 into rd
+                branch_o = 1'b0; //not a branch_o
+                alu_op_o = 2'bxx;// dont care as alu isnt used
+                jump_o = 2'b01;
+    end
+    7'b1101111: begin //jalr
+                reg_write_o = 1'b1;
+                imm_src_o = 3'b111;
+                alu_src_o = 1'b1;
+                mem_write_o = 1'b0;
+                result_src_o = 2'b00; 
+                branch_o = 1'b0; //not a branch_o
+                alu_op_o = 2'b10;
+                jump_o = 2'b10;
+    end 
+    
+
+endcase
+assign pc_src_o= branch_o&zero_i;
 
 endmodule
