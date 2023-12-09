0d2e6909bba3db664d9948acac38dd93f8e173fa
Dell Saxena <dellsaxena2@gmail.com>
2023-12-09 23:34:21 +0000

    fixed sign_extend for J-type instructions

--- Lab4/control/sign_extend.sv
+++ Lab4/control/sign_extend.sv
@@ -13,7 +13,7 @@ module sign_extend #(
             2'b00 : imm_ext = {{20{instr31_20[31]}}, instr31_20[31:20]}; //I-type
             2'b01 : imm_ext = {{20{instr31_20[31]}}, instr31_20[31:25], instr11_7[11:7]}; //S-type
             2'b10 : imm_ext = {{20{instr31_20[31]}}, instr11_7[7], instr31_20[30:25], instr11_7[11:8], 1'b0};//B-type
-            2'b11 : imm_ext = {{12{instr31_20[31]}}, instr19_12[19:12], instr31_20[30:21], 1'b0};//J-type - 21 bit signed imm
+            2'b11 : imm_ext = {{12{instr31_20[31]}}, instr19_12[19:12], instr31_20[20], instr31_20[30:21], 1'b0};//J-type - 21 bit signed imm
             default : imm_ext = {IMM_WIDTH{1'b0}};
         endcase
 
