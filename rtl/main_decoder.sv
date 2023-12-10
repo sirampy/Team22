2beb4be36c4c6ae034b556e78204a6e03bc3ff96
Slayque3n <soph.jayson@gmail.com>
2023-12-10 22:30:57 +0000

    u type DDDED

--- rtl/main_decoder.sv
+++ rtl/main_decoder.sv
@@ -5,7 +5,7 @@ module main_decoder (
     output logic          result_src_o,   // select write input
     output logic          mem_write_o,    // memory write enable
     output logic          alu_src_o,      // select rd2 or imm
-    output logic [1:0]    imm_src_o,      // imm select
+    output logic [2:0]    imm_src_o,      // imm select
     output logic          reg_write_o,    // register write enable
     output logic [1:0]    alu_op_o,        // to input into alu decoder
     output logic          jalr_pc_src_o,
@@ -20,7 +20,7 @@ always_latch
     case (op_i)
         7'b0000011: begin // load
             reg_write_o = 1'b1;
-            imm_src_o = 2'b00;
+            imm_src_o = 3'b000;
             alu_src_o = 1'b1;
             mem_write_o = 1'b0;
             result_src_o = 1'b1;
@@ -31,7 +31,7 @@ always_latch
         end
         7'b0110011: begin // arithmetic operations
             reg_write_o = 1'b1;
-            imm_src_o = 2'b00; 
+            imm_src_o = 3'b000; 
             alu_src_o = 1'b0;
             mem_write_o = 1'b0;
             result_src_o = 1'b0;
@@ -42,7 +42,7 @@ always_latch
         end
         7'b0100011: begin // store
             reg_write_o = 1'b0;
-            imm_src_o = 2'b01;
+            imm_src_o = 3'b001;
             alu_src_o = 1'b1;
             mem_write_o = 1'b1;
             result_src_o = 1'b0;
@@ -54,7 +54,7 @@ always_latch
         end
         7'b0010011: begin // I-type ALU
             reg_write_o = 1'b1;
-            imm_src_o = 2'b00;
+            imm_src_o = 3'b000;
             alu_src_o = 1'b1;
             mem_write_o = 1'b0;
             result_src_o = 1'b0;
@@ -66,7 +66,7 @@ always_latch
         end
         7'b1100011: begin // branch
             reg_write_o = 1'b0;
-            imm_src_o = 2'b10;
+            imm_src_o = 3'b010;
             alu_src_o = 1'b0;
             mem_write_o = 1'b0;
             result_src_o = 1'b0; 
@@ -79,7 +79,7 @@ always_latch
 
         7'b1101111: begin // JAL
             reg_write_o = 1'b1;
-            imm_src_o = 2'b00;
+            imm_src_o = 3'b000;
             alu_src_o = 1'b1; 
             mem_write_o = 1'b0;
             result_src_o = 1'b0; 
@@ -91,7 +91,7 @@ always_latch
         end
         7'b1100111: begin // JALR
             reg_write_o = 1'b1;
-            imm_src_o = 2'b00; 
+            imm_src_o = 3'b000; 
             alu_src_o = 1'b1; 
             mem_write_o = 1'b0;
             result_src_o = 1'b0; 
@@ -101,9 +101,35 @@ always_latch
             jalr_pc_src_o = 1'b0;
 
         end
+
+        7'b0010111: begin // U-instruction -- auipc
+            reg_write_o = 1'b1;
+            imm_src_o = 3'b100; 
+            alu_src_o = 1'b0; 
+            mem_write_o = 1'b0;
+            result_src_o = 1'b1; 
+            branch = 1'b1;
+            alu_op_o = 2'b00; 
+            jal = 1'b0;
+            jalr_pc_src_o = 1'b0;
+
+        end
+          7'b0110111: begin // U-instruction -- lui
+            reg_write_o = 1'b1;
+            imm_src_o = 3'b100; 
+            alu_src_o = 1'b0; 
+            mem_write_o = 1'b0;
+            result_src_o = 1'b0; 
+            branch = 1'b0;
+            alu_op_o = 2'b10; 
+            jal = 1'b0;
+            jalr_pc_src_o = 1'b0;
+
+        end
+
         default: begin
             reg_write_o = 1'b0;
-            imm_src_o = 2'b00;
+            imm_src_o = 3'b000;
             alu_src_o = 1'b0;
             mem_write_o = 1'b0;
             result_src_o = 1'b0;
--- rtl/sign_extend.sv
+++ rtl/sign_extend.sv
@@ -10,10 +10,11 @@ module sign_extend #(
 
     always_comb 
         case (imm_src)
-            2'b00 : imm_ext = {{20{instr31_20[31]}}, instr31_20[31:20]}; //I-type
-            2'b01 : imm_ext = {{20{instr31_20[31]}}, instr31_20[31:25], instr11_7[11:7]}; //S-type
-            2'b10 : imm_ext = {{20{instr31_20[31]}}, instr11_7[7], instr31_20[30:25], instr11_7[11:8], 1'b0};//B-type
-            2'b11 : imm_ext = {{12{instr31_20[31]}}, instr19_12[19:12], instr31_20[20], instr31_20[30:21], 1'b0};//J-type - 21 bit signed imm
+            3'b000 : imm_ext = {{20{instr31_20[31]}}, instr31_20[31:20]}; //I-type
+            3'b001 : imm_ext = {{20{instr31_20[31]}}, instr31_20[31:25], instr11_7[11:7]}; //S-type
+            3'b010 : imm_ext = {{20{instr31_20[31]}}, instr11_7[7], instr31_20[30:25], instr11_7[11:8], 1'b0};//B-type
+            3'b011 : imm_ext = {{12{instr31_20[31]}}, instr19_12[19:12], instr31_20[20], instr31_20[30:21], 1'b0};//J-type - 21 bit signed imm
+            3'b100 : imm_ext = {instr31_20[31:20], instr19_12[19]} //U-type
             default : imm_ext = {IMM_WIDTH{1'b0}};
         endcase
 
