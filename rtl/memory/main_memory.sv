001182caa598d8e0054f1ab0a65147c86bb63d35
Mateusz <matpietrzkiewicz@gmail.com>
2023-12-10 16:22:21 +0000

    Tidied up memory in accordance to ALU files

--- rtl/memory/main_memory.sv
+++ rtl/memory/main_memory.sv
@@ -7,27 +7,29 @@ module main_memory # (
 
 ) (
 
-    input logic                             clk,            // Clock
+    input logic                           clk_i,          // Clock
 
-    input logic[ ADDRESS_WIDTH - 1 : 0 ]    address_i,      // Target address
-    input logic                             write_enable_i, // [0] - Do not write value, [1] - Write value
-    input logic[ DATA_WIDTH - 1 : 0 ]       write_value_i,  // Value to write to address if write_enable == 1
+    input logic [ ADDRESS_WIDTH - 1 : 0 ] address_i,      // Target address
+    input logic                           write_enable_i, // Write enable
+    input logic [ DATA_WIDTH - 1 : 0 ]    write_value_i,  // Value to write
 
-    output logic[ DATA_WIDTH - 1 : 0]       read_value_o    // Value read from address
+    output logic [ DATA_WIDTH - 1 : 0]    read_value_o    // Value read at address
 
 );
 
-logic[ 7 : 0 ] memory_bytes[ 2 ** ACTUAL_ADDRESS_WIDTH ];
+logic [ 7 : 0 ] memory_bytes [ 2 ** ACTUAL_ADDRESS_WIDTH - 1 : 0 ];
 
 // The following is done to fix bit length errors
 function logic[ ACTUAL_ADDRESS_WIDTH - 1 : 0 ] convert_address (input logic[ ADDRESS_WIDTH - 1 : 0 ] in);
     convert_address = in[31:16] + in[15:0];
 endfunction
 
+/*
 initial begin
         $display("Loading main memory");
-        $readmemh("memory/sinerom.mem", memory_bytes);
+        $readmemh("memory/sinerom.mem", memory_bytes); // Update to new file, if needed
 end
+*/
 
 always_ff @(posedge clk, posedge write_enable_i)
     if (write_enable_i)
