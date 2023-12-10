d794d7be84bedb32fe09ead5596a4723097d7507
Mateusz <matpietrzkiewicz@gmail.com>
2023-12-10 22:59:16 +0000

    Tidied up some control code

--- rtl/control_top.sv
+++ rtl/control_top.sv
@@ -5,9 +5,6 @@ module control_top #(
 
 ) (
 
-    input  logic [ 6 : 0 ]  op_i,                               // Note: Delete. We have instruction already.
-    input  logic [ 2 : 0 ]  funct3_i,                           // Note: Delete. We have instruction already.
-    input  logic            funct7_i,                           // Note: Delete. We have instruction already.
     input  logic [ 31 : 0 ] pc_i,                          // Program counter
     input  logic            eq_i,                          // Equal/zero flag
 
@@ -19,7 +16,7 @@ module control_top #(
     output logic                            jalr_pc_src_o, // [0] - ?, [1] - ?
     output logic [ 3 : 0 ]                  alu_ctrl_o,    // ALU operation select
     output logic [ 31 : 0 ]                 imm_op_o,      // Immediate value
-    output logic [ INSTR_WIDTH - 1 : 0 ]    instr_o,       // Current instruction to execute 
+    output logic [ INSTR_WIDTH - 1 : 0 ]    instr_o,       // Current instruction to execute
     output logic [ REG_ADDR_WIDTH - 1 : 0 ] rs1_o,              // ALU operand register address 1
     output logic [ REG_ADDR_WIDTH - 1 : 0 ] rs2_o,              // ALU operand register address 2
     output logic [ REG_ADDR_WIDTH - 1 : 0 ] rd_o                // Register write address
@@ -28,11 +25,18 @@ module control_top #(
 
 logic [ 1 : 0 ] alu_op;  // [00] - LW/SW, [01] - B-type, [10] - Mathematical expression (R-type or I-type)
 logic [ 1 : 0 ] imm_src; // Immediate value type
+logic [ 6 : 0 ] op,      // Instruction operand
+logic [ 2 : 0 ] funct3,  // Operator select
+logic           funct7,  // Operator select
 
 
 assign rs1 = instr_o [ 19 : 15 ];      // Move to top
 assign rs2 = instr_o [ 24 : 20 ];      // Move to top
 assign rd = instr_o [ 11 : 7 ];        // Move to top
+
+assign op = instr_o [ 6 : 0 ];
+assign funct3 = instr_o [ 14 : 12 ];
+assign funct7 = instr_o [ 30 ];
 
 instr_mem instr_mem (
 
@@ -43,29 +47,29 @@ instr_mem instr_mem (
 
 main_decoder main_decoder (
 
-    .eq_i (eq_i),
-    .op_i (op_i),
-    .funct3_i (funct3_i)
+    .eq_i          (eq_i),
+    .op_i          (op),
+    .funct3_i      (funct3),
 
-    .pc_src_o (pc_src_o),
-    .result_src_o (result_src_o),
-    .mem_write_o (mem_write_o),
-    .alu_src_o (alu_src_o),
-    .imm_src_o (imm_src),
-    .reg_write_o (reg_write_o),
+    .pc_src_o      (pc_src_o),
+    .result_src_o  (result_src_o),
+    .mem_write_o   (mem_write_o),
+    .alu_src_o     (alu_src_o),
+    .imm_src_o     (imm_src),
+    .reg_write_o   (reg_write_o),
     .jalr_pc_src_o (jalr_pc_src_o),
-    .alu_op_o (alu_op),
+    .alu_op_o      (alu_op)
 
 );
 
 alu_decoder alu_decoder (
 
-    .op5_i (op_i[5]),
-    .funct3_i (funct3_i),
-    .funct7_i (funct7_i),
-    .alu_op_i (alu_op),
+    .funct3_i      ( funct3 ),
+    .funct7_i      ( funct7 ),
+    .op5_i         ( op [ 5 ] ),
+    .alu_op_i      ( alu_op ),
 
-    .alu_control_o (alu_ctrl_o)
+    .alu_control_o ( alu_ctrl_o )
 
 );
 
