0ada599adf3b9e6c73687d942e02deb8b5c6c862
Mateusz <matpietrzkiewicz@gmail.com>
2023-12-10 23:12:23 +0000

    Tidied up instruction memory

--- rtl/instr_mem.sv
+++ rtl/instr_mem.sv
@@ -12,16 +12,18 @@ module instr_mem #(
 
 );
 
-function logic [ ACTUAL_ADDRESS_WIDTH - 1 : 0 ] convert_address (input logic [ ADDRESS_WIDTH - 1 : 0 ] in);
-    convert_address = in[ 31 : 16 ] + in[ 15 : 0 ]; // Addition is performed to prevent errors involving unused bits
+function logic [ ACTUAL_ADDRESS_WIDTH - 1 : 0 ] convert_address ( input logic [ ADDRESS_WIDTH - 1 : 0 ] in );
+    convert_address = in [ 31 : 16 ] + in [ 15 : 0 ]; // Addition is performed to prevent errors involving unused bits
 endfunction
 
 logic [ 8 : 0 ] byte_array [ ( 2 ** ACTUAL_ADDRESS_WIDTH ) - 1 : 0 ];
 
+/*
 initial begin
         $display("Loading instructions into ROM.");
         $readmemh("control/program.mem", byte_array); // Choose correct .mem location
 end;
+*/
 
 assign rd_o = { byte_array[ convert_address( addr_i ) + 3 ], // Little endian. May want to change if loaded instructions are big endian?
                 byte_array[ convert_address( addr_i ) + 2 ],
