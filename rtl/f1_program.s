431ca6744e53cd614bd9ea43d258b3d6f8343c20
Slayque3n <soph.jayson@gmail.com>
2023-12-11 14:45:30 +0000

    f1 lights in branch

--- rtl/f1_program.s
+++ rtl/f1_program.s
@@ -1,28 +1,30 @@
 # F1 Program Implementation - Assembly Language
 init:
-    li s1, 0x0 # vbd flag value
-    li s2, 0xFF # all lights are on
-    li s3, 0x10 # need to adjust to get 1s delay
-
+    ADDI a1, zero, 0 #vbd flag value - trigger
+    ADDI a2, zero, 11111111 # all lights are on
+    ADDI a3, zero, 00001111 # need to adjust to get 1s delay
+    ADDI a4, zero, 0 # reset the count for light delay
+    ADDI a5, zero, 0  #final output for turning on lights
 main:
-    beq s1, zero, main # loop until s1 not equal 0 - ie vbdflag pressed
-    jal ra, lights_loop
-    jal ra, main
+    BEQ a1, zero, main # loop until s1 not equal 0 - ie vbdflag pressed
+    JAL ra, lights_loop
+    JAL ra, main
     
 lights_loop:
-    jal ra, lightdelay # 1s delay
-    slli a0, a0, 0x1 # shifts t0 left
-    addi a0, a0, 0x1 # adds 1 to t0
-    bne  a0, s2, lights_loop # loops until all lights are on
-    jal ra, turn_off
-    ret
+    JAL ra, lightdelay # 1s delay
+    SLLI a5, a5, 1 # shifts t0 left
+    ADDI a5, a5, 1 # adds 1 to t0
+    BNE  a0, s2, lights_loop # loops until all lights are on
+    JAL ra, turn_off
+    RET
 
 lightdelay:
-    addi  a1, a1, 0x1 # a1 acts as counter
-    bne   a1, s3, lightdelay
-    addi  a1, zero, 0x0
+    ADDI  a4, a4, 1 # a4 acts as counter
+    BNE   a1, a3, lightdelay
+    ADDI  a1, zero, 0
+
 
 turn_off: # wait some delay and turn off all lights
-    jal ra, lightdelay
-    li a0, 0x0
-    ret
+    JAL ra, lightdelay
+    ADDI a5, zero, 0  #switch lights offf
+    RET
