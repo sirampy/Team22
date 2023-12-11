fd3070d37874a656f3506795aba875916d602e11
bethcham <bethcham@gmail.com>
2023-12-11 17:14:07 +0000

    Pulled changes from pipelining

--- Project/control/ext.sh
+++
@@ -1,8 +0,0 @@
-rm -rf obj_dir
-rm -f controlunit.vcd
-
-verilator -Wall --cc --trace  controlunit.sv --exe control_tb.cpp
-
-make -j -C obj_dir/ -f Vcontrolunit.mk Vcontrolunit
-
-obj_dir/Vcontrolunit
