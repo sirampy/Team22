cf673b932c0ebf1834b3d6f1607b387c5cf119fb
Slayque3n <soph.jayson@gmail.com>
2023-12-12 14:25:22 +0000

    f1 lights (need to commit to switch branches )

--- test/f1/f1.s
+++ test/f1/f1.s
@@ -1,19 +1,20 @@
 # F1 Program Implementation - Assembly Language
 init:
-    ADDI a1, a0, 0              # vbd flag value - trigger
-    ADDI a2, a0, 255            # all lights are on - written in decimal
-    ADDI a3, a0, 5       # TODO: need to adjust to get 1s delay
-    ADDI a4, a0, 0              # reset the count for light delay
-    ADDI a5, a0, 0              # final output for turning on lights
+    ADDI a1, zero, 0              # vbd flag value - trigger
+    ADDI a2, zero, 255            # all lights are on - written in decimal
+    ADDI a3, zero, 5       # TODO: need to adjust to get 1s delay
+    ADDI a4, zero, 0              # reset the count for light delay
+    ADDI a5, zero, 0              # final output for turning on lights
 main:
     #BEQ a1, a0, main            
     # loop until s1 not equal 0 - ie vbdflag pressed
     #TRIGGER REMOVED FOR TESTING
     JAL ra, lights_loop
-    BEQ a0,a0,init
+    BEQ zero,zero,init
     
 lights_loop:
     JAL ra, lightdelay            # 1s delay
+    ADDI a4, zero, 0              # reset the count for light delay
     SLLI a5, a5, 1                # shifts t0 left
     ADDI a5, a5, 1                # adds 1 to t0
     BNE  a5, a2, lights_loop      # loops until all lights are on
