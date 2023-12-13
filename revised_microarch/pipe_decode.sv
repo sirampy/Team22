c87b2f00869e62301ac3cf5bc5c0bad82004b198
Sirampy <alex.ch4.cl@gmail.com>
2023-12-13 14:17:43 +0000

    pipe decode upgrade

--- revised_microarch/pipe_decode.sv
+++ revised_microarch/pipe_decode.sv
@@ -15,9 +15,23 @@ module decode_fetch #(
     input funct3_t funct3_i,
     input next_pc_t pc_control_i,
 
-    input logic [31:0] reg_data_2_i
+    input srcr_t srcr_i,
+    input [31:0] pc_inced_i,
+    input [3:0]  rd_i,
+    input logic reg_write_i,
+    input logic data_read_i,
+    input logic data_write_i,
+    input next_pc_t pc_control_i,
+    input logic [31:0] reg_data_2_i,
 
-    output logic [31:0] reg_data_2_o
+    output srcr_t srcr_o,
+    output [31:0] pc_inced_o,
+    output [3:0]  rd_o,
+    output logic reg_write_o,
+    output logic data_read_o,
+    output logic data_write_o,
+    output next_pc_t pc_control_o,
+    output logic [31:0] reg_data_2_o,
 
     output logic [4:0] alu_src_1_o,
     output logic [4:0] alu_src_2_o,
@@ -40,6 +54,13 @@ always_ff @(posedge clk_i)
                 fucnt3_o <= funct3_i;  
                 pc_control_o <= pc_control_i;
 
+                srcr_o <= srcr_o;
+                pc_inced_o <= pc_inced_i;
+                rd_o <= rd_i;
+                reg_write_o <= reg_write_o;
+                data_read_o <= data_read_i;
+                data_write_o <= data_write_i;
+                pc_control_o <= pc_control_i;
                 reg_data_2_o <= reg_data_2_i;
 
             end
@@ -57,6 +78,9 @@ always_ff @(posedge clk_i)
                 fucnt3_o <= alu.I_STD;  
                 pc_control_o <= NEXT_PC;
                 reg_data_2_o <= 'b0;
+
+                reg_write_o <= 0;
+                data_write_o <= 0;
             end
 
             default: display("fetch: unknown pipeline control signal");
