b42c0458686502a3294b34c7f3da4e8b910ff521
sirampy <60194718+sirampy@users.noreply.github.com>
2023-12-14 20:28:56 +0000

    Update data_mem.sv
    naming fixed

--- revised_microarch/data_mem.sv
+++ revised_microarch/data_mem.sv
@@ -16,30 +16,30 @@ logic [31:0] rd_wrd;
 
 assign a_resized = a_i[ADDRESS_WIDTH-1:0];
 
-reg [DATA_WIDTH-1:0] rom_array [2**ADDRESS_WIDTH-1:0];
+reg [DATA_WIDTH-1:0] ram_array [2**ADDRESS_WIDTH-1:0];
 
 initial begin
-        $display("loading rom.");
-        $readmemh("rom_bin/data.mem", rom_array);
+        $display("loading ram.");
+        $readmemh("ram_bin/data.mem", ram_array);
 end;
 
 always_latch begin
 
     if (wen_i) begin
         case(load3_i)
-            BYTE: rom_array[a_resized] = wd_i[7:0];
-            HALF: {rom_array[a_resized+1],rom_array[a_resized]} = wd_i[15:0];
-            WORD: {rom_array[a_resized+3],rom_array[a_resized+2],rom_array[a_resized+1],rom_array[a_resized]} = wd_i;
+            BYTE: ram_array[a_resized] = wd_i[7:0];
+            HALF: {ram_array[a_resized+1],ram_array[a_resized]} = wd_i[15:0];
+            WORD: {ram_array[a_resized+3],ram_array[a_resized+2],ram_array[a_resized+1],ram_array[a_resized]} = wd_i;
         
             default: $display("trying to write with invalid write opc");
         endcase
     end
 
     // load operation
-    rd_wrd = {rom_array[a_resized+3],
-        rom_array[a_resized+2],
-        rom_array[a_resized+1],
-        rom_array[a_resized]}; // TODO: is this the right way round? 
+    rd_wrd = {ram_array[a_resized+3],
+        ram_array[a_resized+2],
+        ram_array[a_resized+1],
+        ram_array[a_resized]}; // TODO: is this the right way round? 
     
     case (load3_i)
         BYTE: rd_o = {{24{rd_wrd[7]}}, rd_wrd[7:0]};
