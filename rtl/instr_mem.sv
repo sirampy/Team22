1cb650921b28d5d62d85a827aa7458de62e48ea0
Dell Saxena <dellsaxena2@gmail.com>
2023-12-14 16:16:36 +0000

    updated instr_mem.sv

--- rtl/instr_mem.sv
+++ rtl/instr_mem.sv
@@ -2,8 +2,6 @@ module instr_mem #(
     
     parameter ADDRESS_WIDTH = 32,
     parameter DATA_WIDTH = 32
-   // parameter ACTUAL_ADDRESS_WIDTH = 16 // Needed to prevent errors involved with making array of size 2 ** 32
-                                        // We just take it as 2 ** 16, as that works in the scope of the lab
 )(
 
     input logic [ ADDRESS_WIDTH - 1 : 0 ] addr_i, // Address to read from
@@ -20,7 +18,7 @@ module instr_mem #(
     end;
 
     always_comb begin 
-    rd_o = {rom_array[(addr_i + 3)],rom_array[(addr_i + 2) ],rom_array[(addr_i+ 1) ],rom_array[addr_i ]}; 
+    rd_o = {rom_array[(addr_i + 3)],rom_array[(addr_i + 2) ],rom_array[(addr_i+ 1)],rom_array[addr_i]}; 
     end
 
 endmodule
