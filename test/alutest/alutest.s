8798c16ca2d0d85f8cf80ebd5d82b64bc3a0c31c
bethcham <bethcham@gmail.com>
2023-12-14 05:43:51 +0000

    Debuging LBU

--- test/alutest/alutest.s
+++ test/alutest/alutest.s
@@ -1,5 +1,10 @@
  ADDI a1, zero, 10
+ ADDI a1, zero, 0
+ LUI a1, 0x00100
+ SB      zero, x10000(a1)  #     mem[base_pdf+a1) = 0
  ADDI a2, zero, 5
+ LUI  a1, 0x00000
+ LBU  a0, x10000(a1)
  LUI  a1, 0x11111
  ADD a1, a1, a0
  LUI  a2, 255             # a2 = max index of pdf array
