9e2baf4d33f670e699bcaab5cb3314a99cf45027
Slayque3n <soph.jayson@gmail.com>
2023-12-15 18:50:23 +0000

    testing

---
+++ rom_bin/alutest.asm
@@ -0,0 +1,30 @@
+
+rom_bin/alutest-reloc.out:     file format elf32-littleriscv
+
+
+Disassembly of section .text:
+
+bfc00000 <__BSS_END__-0x1010> (File Offset: 0x1000):
+bfc00000:	00100593          	addi	a1,zero,1
+bfc00004:	00100613          	addi	a2,zero,1
+bfc00008:	00c595b3          	sll	a1,a1,a2
+bfc0000c:	00159593          	slli	a1,a1,0x1
+
+Disassembly of section .riscv.attributes:
+
+00000000 <.riscv.attributes> (File Offset: 0x1010):
+   0:	1e41                	.2byte	0x1e41
+   2:	0000                	.2byte	0x0
+   4:	7200                	.2byte	0x7200
+   6:	7369                	.2byte	0x7369
+   8:	01007663          	bgeu	zero,a6,14 <__BSS_END__-0xbfc00ffc> (File Offset: 0x1024)
+   c:	0014                	.2byte	0x14
+   e:	0000                	.2byte	0x0
+  10:	7205                	.2byte	0x7205
+  12:	3376                	.2byte	0x3376
+  14:	6932                	.2byte	0x6932
+  16:	7032                	.2byte	0x7032
+  18:	5f30                	.2byte	0x5f30
+  1a:	326d                	.2byte	0x326d
+  1c:	3070                	.2byte	0x3070
+	...
